fc0a1c15483d70b952bfa469984000b417b02565
devprince101 <75951797+devprince101@users.noreply.github.com>
2024-04-01 11:16:46 +0000

    Finalization
    Finally

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import './styles/contact.css'
+import Greencover from './crop.jpg';
 
 function Contact() {
   return (
     <section id='contact'>
-        <div>
-            <h1>Ready to Connect?</h1>
-            <div>
-                <small>we would like to hear from you!</small>
-                <div>
-                    <h2>AfroGreen Limited</h2>
-                    <ul>
+        <div className='cowrapper'>
+            <h1 className='cohe'>Ready to Connect?</h1>
+            <div className='cocont'>
+                <small className='cosmall'>we would like to hear from you!</small>
+                <div className='cogrid'>
+                    <div className='coheads'>
+                        <h2 className='cohead'>AfroGreen Limited</h2>
+                        <img className='coima' src={Greencover} alt='Green Cover'/>
+                        <small className='cooo'>&copy; AfroGreen Limited. All Rights Reserved!</small>
+                    </div>
+                    <ul className='cogreet'>
                         <li><strong>Location: </strong> Nakuru County, KE 20108</li>
                         <li><strong>Tel: </strong> +2547500066990</li>
                         <li><strong>Email: </strong>afrogreenltd@afrogreen.com</li>
